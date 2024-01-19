3baf2af794501ad0b884711b19130c29392d286d
Henrique de Cara <henrique.decara@windriver.com>
2024-01-19 17:47:16 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function iSPalindrome (word) {
+    
+    const chars = word.split("");
+    let comp  = word.length
+    let palindrome_count = 0;
+    
+
+    for (let i = 0; i < (comp); i++) {
+        if (chars[i] == chars[(comp-1)-i]) {
+            palindrome_count++;
+        }
+    }
+    if (palindrome_count == comp){
+        console.log("Palindrome")
+    }
+    else{
+        console.log("Not Palindrome")
+    }
+    }
+
+
+iSPalindrome("traxart")
